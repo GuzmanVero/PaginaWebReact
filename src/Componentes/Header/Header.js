28582c78e0e6fbf0958c9abb9f5504da7fd5b730
GuzmanVero <marcyguz19@gmail.com>
2023-10-06 23:43:25 -0600

    intento de nav responsive

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logoC from './cropped-tweb.png';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
-function Header() {
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
     return (
-      <header>
+      <div className='nav'>
         <div className="logo">
           <img width="110" height="117" className="logo" src={logoC} alt="LogoEmpresa" />
           <p><h2>Citas Medicas </h2>
           <br/>Los mejores servicios y atencion medica </p>      
         </div> 
-        <div className="menuheader">
-          <table className="menu" border="1" cellpadding="5">
-            <tr>
-            <td><Link to="/" className="enlaceheader" style={{textDecoration:"none"}}>Inicio</Link></td>
-            <td><Link to="/About" className="enlaceheader" style={{textDecoration:"none"}}>Nosotros</Link></td>
-            <td><Link to="/Services" className="enlaceheader" style={{textDecoration:"none"}}>Servicios</Link></td>
-            <td><Link to="/Contact" className="enlaceheader" style={{textDecoration:"none"}}>Contacto</Link></td>
-            {/*<td><Link to="/SignUp" className="enlaceheader" style={{textDecoration:"none"}}>Registro</Link></td>*/}
-            <td><SigupButton/></td>
-            <td><LoginButton/></td>
-            </tr>
-        </table>           
-      </div>   
-       
-    </header>
+        <div className={`menuheader ${isOpen && "open"}`}>
+
+          <Link to="/" className="enlaceheader" style={{textDecoration:"none"}}>Inicio</Link>
+
+          <Link to="/About" className="enlaceheader" style={{textDecoration:"none"}}>Nosotros</Link>
+
+          <Link to="/Services" className="enlaceheader" style={{textDecoration:"none"}}>Servicios</Link>
+
+          <Link to="/Contact" className="enlaceheader" style={{textDecoration:"none"}}>Contacto</Link>
+
+          {/*<td><Link to="/SignUp" className="enlaceheader" style={{textDecoration:"none"}}>Registro</Link></td>*/}
+          <SigupButton/>
+          <LoginButton/> 
+      </div>
+      <div className='nav_toggle' onClick={ () => setIsOpen(!isOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+    </div>
   );
 }
 
